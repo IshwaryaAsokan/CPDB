b0424fa69fac97b77543245df8ba050b7da1844a
ISH <ish@probosys.com>
2016-08-23 14:31:19 +0530

    PDS 14 - Added Cross Selling functionality to CPDB Domain Service

--- src/com/probosys/file/utils/FileUtils.java
+++ src/com/probosys/file/utils/FileUtils.java
@@ -14,7 +14,6 @@ import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.poi.POIXMLException;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -33,12 +32,13 @@ public class FileUtils {
 
 			// Get the workbook instance for XLS file
 			HSSFWorkbook workbook = new HSSFWorkbook(fileStream);
-			
-			try{
-				String fileName = "D:/uploadedfiles/xls_"+DateFormatUtils.format(new Date(),"yyyyMMdd_HHmmss");
-				
+
+			try {
+				String fileName = "D:/uploadedfiles/xls_"
+						+ DateFormatUtils.format(new Date(), "yyyyMMdd_HHmmss");
+
 				FileUtils.writeXlsFile(workbook, fileName);
-			}catch(Exception ex){
+			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
 			// Get first sheet from the workbook
@@ -51,7 +51,7 @@ public class FileUtils {
 				Row row = rowIterator.next();
 				if (cnt == 0) {
 					cnt++;
-				}else{
+				} else {
 					PimPojo pojo = new PimPojo();
 					Cell c = row.getCell(0);
 					if (Cell.CELL_TYPE_BLANK != c.getCellType()) {
@@ -62,12 +62,13 @@ public class FileUtils {
 						pojo.setChild(c.getStringCellValue());
 					}
 					c = row.getCell(2);
-					if (Cell.CELL_TYPE_BLANK != c.getCellType() && c.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+					if (Cell.CELL_TYPE_BLANK != c.getCellType()
+							&& c.getCellType() == Cell.CELL_TYPE_NUMERIC) {
 						Double val = c.getNumericCellValue();
 						pojo.setStatusId(val != null ? val.longValue() : 0);
-					}else if(Cell.CELL_TYPE_BLANK != c.getCellType()){
+					} else if (Cell.CELL_TYPE_BLANK != c.getCellType()) {
 						String val = c.getStringCellValue();
-						pojo.setStatusId(val != null ? Long.parseLong(val): 0 );
+						pojo.setStatusId(val != null ? Long.parseLong(val) : 0);
 					}
 					c = row.getCell(3);
 					if (Cell.CELL_TYPE_BLANK != c.getCellType()) {
@@ -88,6 +89,78 @@ public class FileUtils {
 		return pojoList;
 	}
 
+	public static List<PimPojo> readCSXlFile(InputStream fileStream) {
+
+		int cnt = 0;
+		List<PimPojo> pojoList = new ArrayList<>();
+
+		// FileInputStream fileStream = new FileInputStream(file);
+
+		// Get the workbook instance for XLS file
+
+		try {
+			XSSFWorkbook workbook = new XSSFWorkbook(fileStream);
+
+			// Get first sheet from the workbook
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			Iterator<Row> rowIterator = sheet.iterator();
+			// int cnt=0;
+
+			while (rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+				if (cnt == 0) {
+					cnt++;
+				} else {
+					PimPojo pojo = new PimPojo();
+					Cell c = row.getCell(0);
+					if (Cell.CELL_TYPE_BLANK != c.getCellType()
+							&& c.getCellType() == Cell.CELL_TYPE_STRING) {
+						pojo.setParent(c.getStringCellValue());
+					} else if (Cell.CELL_TYPE_BLANK != c.getCellType()
+							&& c.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+						pojo.setParent(c.getNumericCellValue() + "");
+					}
+					
+					c = row.getCell(1);
+					if (Cell.CELL_TYPE_BLANK != c.getCellType()
+							&& c.getCellType() == Cell.CELL_TYPE_STRING) {
+						pojo.setCrossSellChild(c.getStringCellValue());
+					} else if (Cell.CELL_TYPE_BLANK != c.getCellType()
+							&& c.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+						pojo.setCrossSellChild(c.getNumericCellValue() + "");
+					}
+					
+					c = row.getCell(2);
+					
+					if (Cell.CELL_TYPE_BLANK != c.getCellType()
+							&& c.getCellType() == Cell.CELL_TYPE_STRING) {
+						pojo.setCrossSellType(c.getStringCellValue());
+					} else if (Cell.CELL_TYPE_BLANK != c.getCellType()
+							&& c.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+						pojo.setCrossSellType(c.getNumericCellValue() + "");
+					}
+					
+					c = row.getCell(3);
+					if (Cell.CELL_TYPE_BLANK != c.getCellType()) {
+						pojo.setAction(c.getStringCellValue());
+					}
+					pojoList.add(pojo);
+				}
+			}
+		} catch (POIXMLException ife) {
+			ife.printStackTrace();
+			throw new RuntimeException("File Not Found. Kindly upload file");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("File Not Found. Kindly upload file");
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("File Not Found. Kindly upload file");
+		}
+
+		return pojoList;
+	}
+
 	public static List<PimPojo> readXslsFile(InputStream fileStream) {
 		List<PimPojo> pojoList = new ArrayList<>();
 		try {
@@ -101,7 +174,7 @@ public class FileUtils {
 			XSSFSheet sheet = workbook.getSheetAt(0);
 
 			Iterator<Row> rowIterator = sheet.iterator();
-			int cnt=0;
+			int cnt = 0;
 			while (rowIterator.hasNext()) {
 				Row row = rowIterator.next();
 				if (cnt == 0) {
@@ -110,30 +183,37 @@ public class FileUtils {
 				}
 				PimPojo pojo = new PimPojo();
 				Cell c = row.getCell(0);
-				if (Cell.CELL_TYPE_BLANK != c.getCellType() && c.getCellType() == Cell.CELL_TYPE_STRING) {
+				if (Cell.CELL_TYPE_BLANK != c.getCellType()
+						&& c.getCellType() == Cell.CELL_TYPE_STRING) {
 					pojo.setParent(c.getStringCellValue());
-				}else  if(Cell.CELL_TYPE_BLANK != c.getCellType() && c.getCellType() == Cell.CELL_TYPE_NUMERIC){
+				} else if (Cell.CELL_TYPE_BLANK != c.getCellType()
+						&& c.getCellType() == Cell.CELL_TYPE_NUMERIC) {
 					pojo.setParent(c.getNumericCellValue() + "");
 				}
-				
+
 				c = row.getCell(1);
-				if (Cell.CELL_TYPE_BLANK != c.getCellType() && c.getCellType() == Cell.CELL_TYPE_STRING) {
+				if (Cell.CELL_TYPE_BLANK != c.getCellType()
+						&& c.getCellType() == Cell.CELL_TYPE_STRING) {
 					pojo.setChild(c.getStringCellValue());
-				}else  if(Cell.CELL_TYPE_BLANK != c.getCellType() && c.getCellType() == Cell.CELL_TYPE_NUMERIC){
+				} else if (Cell.CELL_TYPE_BLANK != c.getCellType()
+						&& c.getCellType() == Cell.CELL_TYPE_NUMERIC) {
 					pojo.setChild(c.getNumericCellValue() + "");
 				}
 				c = row.getCell(2);
-				if (Cell.CELL_TYPE_BLANK != c.getCellType() && c.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+				if (Cell.CELL_TYPE_BLANK != c.getCellType()
+						&& c.getCellType() == Cell.CELL_TYPE_NUMERIC) {
 					Double val = c.getNumericCellValue();
 					pojo.setStatusId(val != null ? val.longValue() : 0);
-				}else if(Cell.CELL_TYPE_BLANK != c.getCellType()){
+				} else if (Cell.CELL_TYPE_BLANK != c.getCellType()) {
 					String val = c.getStringCellValue();
-					pojo.setStatusId(val != null ? Long.parseLong(val): 0 );
+					pojo.setStatusId(val != null ? Long.parseLong(val) : 0);
 				}
 				c = row.getCell(3);
-				if (Cell.CELL_TYPE_BLANK != c.getCellType() && c.getCellType() == Cell.CELL_TYPE_STRING) {
+				if (Cell.CELL_TYPE_BLANK != c.getCellType()
+						&& c.getCellType() == Cell.CELL_TYPE_STRING) {
 					pojo.setItemType(c.getStringCellValue());
-				}else  if(Cell.CELL_TYPE_BLANK != c.getCellType() && c.getCellType() == Cell.CELL_TYPE_NUMERIC){
+				} else if (Cell.CELL_TYPE_BLANK != c.getCellType()
+						&& c.getCellType() == Cell.CELL_TYPE_NUMERIC) {
 					pojo.setItemType(c.getNumericCellValue() + "");
 				}
 				pojoList.add(pojo);
@@ -163,7 +243,7 @@ public class FileUtils {
 			 * FileOutputStream out = new FileOutputStream(new
 			 * File("C:\\test.xls")); workbook.write(out); out.close();
 			 */
-		}catch(POIXMLException ife){
+		} catch (POIXMLException ife) {
 			ife.printStackTrace();
 			throw new RuntimeException("File Not Found. Kindly upload file");
 		} catch (FileNotFoundException e) {
@@ -173,20 +253,22 @@ public class FileUtils {
 			e.printStackTrace();
 			throw new RuntimeException("File Not Found. Kindly upload file");
 		}
-	/*	for (PimPojo pimPojo : pojoList) {
-			System.out.println(pimPojo.getItemType());
-		}*/
+		/*
+		 * for (PimPojo pimPojo : pojoList) {
+		 * System.out.println(pimPojo.getItemType()); }
+		 */
 		return pojoList;
 	}
-	
-	public static void writeXlsFile(Workbook workBook, String fileName) throws IOException{
-		   //lets write to file
+
+	public static void writeXlsFile(Workbook workBook, String fileName)
+			throws IOException {
+		// lets write to file
 		File file = new File(fileName);
 		file.mkdirs();
-        FileOutputStream fos = new FileOutputStream(fileName);
-        workBook.write(fos);
-        fos.close();
-        System.out.println(fileName + " written successfully");
+		FileOutputStream fos = new FileOutputStream(fileName);
+		workBook.write(fos);
+		fos.close();
+		System.out.println(fileName + " written successfully");
 	}
 
 }
